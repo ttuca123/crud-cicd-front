0fb619548568b46b90e5dcc803638c9a2775f0b9
ttuca123 <ttuca123@gmail.com>
2024-07-03 10:19:30 +0000

    Deploying to gh-pages from  @ 8b4491800e2667de74b37ddaeab61e68669129c3 🚀

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  url: 'https://trabalho-ci-cd.onrender.com',
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  url: 'https://trabalho-ci-cd.onrender.com',
+};
